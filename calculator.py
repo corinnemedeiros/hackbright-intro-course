d2c556023d00e900934b64d89aaa1064a1656bbd
Corinne <corinne.marie08@gmail.com>
2016-05-07 14:25:09 -0700

    updated calculator.py

--- calculator.py
+++ calculator.py
@@ -19,7 +19,37 @@ def power(base, exponent):
 def square(num):
 	return num ** 2
 
-aa = add(4, 5):
-ab = subtract(9, 6):
+aa = add(4, 5)
+ab = subtract(9, 6)
 
 print aa + ab
+
+b = divide(12, 2)
+
+print b - 60
+
+c = add(1, 2)
+
+print c + 3
+
+d = add(1, 2)
+
+print square(d)
+
+e = modulo(3, 4)
+eb = multiply(9, 9)
+
+print divide(e, eb)
+
+f = add(3, 8)
+
+print multiply(7, f)
+
+ga = add(1, 2)
+gb = add(4, 5)
+
+print ga - 3 * gb
+
+h = add(2, 3)
+
+print power(3, h)
