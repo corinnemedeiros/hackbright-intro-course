5c3a9eee687da8de2c3f5ef725fe535d38f5ee1d
Corinne <corinne.marie08@gmail.com>
2016-05-07 14:06:08 -0700

    added calculator.py

---
+++ calculator.py
@@ -0,0 +1,25 @@
+def add(num1, num2):
+	return num1 + num2
+
+def subtract(num1, num2):
+	return num1 - num2
+
+def multiply(num1, num2):
+	return num1 * num2
+
+def divide(num1, num2):
+	return num1 / num2
+
+def modulo(num1, num2):
+	return num1 % num2
+
+def power(base, exponent):
+	return base ** exponent
+
+def square(num):
+	return num ** 2
+
+aa = add(4, 5):
+ab = subtract(9, 6):
+
+print aa + ab
